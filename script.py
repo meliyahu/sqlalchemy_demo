9e0acffa45717f1a8e87e7176dcd24f9fd5fff9c
Mosheh EliYahu <mosheh.eliyahu@gmail.com>
2022-01-03 21:04:56 +1030

    db model

--- script.py
+++ script.py
@@ -4,8 +4,8 @@ import sys
 
 import requests
 
-print(sys.version)
-print(sys.executable)
+# print(sys.version)
+# print(sys.executable)
 
-req = requests.get(url='https://coreyms.com')
+req = requests.get(url='https://ecoimages-test.tern.org.au')
 print(req.status_code)
