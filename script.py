8e6a65e26d1808ad8acb3d828d48c910be2efc2f
Mosheh EliYahu <mosheh.eliyahu@gmail.com>
2022-01-03 15:34:49 +1030

    updates

--- script.py
+++ script.py
@@ -7,18 +7,5 @@ import requests
 print(sys.version)
 print(sys.executable)
 
-
-def greet(who_to_greet):
-    """[summary]
-
-    Args:
-        who_to_greet ([type]): [description]
-
-    Returns:
-        [type]: [description]
-    """
-    greeting = f'Hello, {who_to_greet}'
-    return greeting
-
 req = requests.get(url='https://coreyms.com')
 print(req.status_code)
